be3e4bd2a528eda09682a37e1bc43e946a0d2d1b
vanish <2013107081@qq.com>
2025-06-17 22:11:03 +0800

    feat: Update CI/CD pipeline and configurations
    - Added sparse checkout for ci-defaults.json in the CI/CD pipeline.
    - Implemented fallback logic to read default configurations from ci-defaults.json if input parameters are empty.
    - Commented out release and deploy steps in the CI/CD pipeline for future reference.
    - Created ci-defaults.json with default configurations for metadata, artifacts, and test data.
    - Updated Unity version default in step-1-test.yml to '2022.3.58f1'.
    - Removed unnecessary line in step-2-build.yml.
    - Deleted DOTweenModulePhysics2D.cs and its associated meta files.
    - Commented out Rigidbody2D check in DOTweenModuleUtils.cs.
    - Created test folders and assembly definition files for Editor and PlayMode tests.
    - Cleaned up manifest.json by removing unused Unity modules.
    - Updated packages-lock.json to reflect the removal of certain Unity modules.
    - Modified ProjectSettings.asset to include scripting define symbols for Odin Inspector.

--- Assets/Plugins/Demigiant/DOTween/Modules/DOTweenModuleUtils.cs
+++ Assets/Plugins/Demigiant/DOTween/Modules/DOTweenModuleUtils.cs
@@ -96,14 +96,14 @@ namespace DG.Tweening
             }
 
             // Returns FALSE if the DOTween's Physics2D Module is disabled, or if there's no Rigidbody2D attached
-            public static bool HasRigidbody2D(Component target)
-            {
-#if true // PHYSICS2D_MARKER
-                return target.GetComponent<Rigidbody2D>() != null;
-#else
-                return false;
-#endif
-            }
+//             public static bool HasRigidbody2D(Component target)
+//             {
+// #if true // PHYSICS2D_MARKER
+//                 return target.GetComponent<Rigidbody2D>() != null;
+// #else
+//                 return false;
+// #endif
+//             }
 
             #region Called via Reflection
 
@@ -122,44 +122,44 @@ namespace DG.Tweening
 #endif
             }
 
-            // Called via Reflection by DOTweenPath
-#if UNITY_2018_1_OR_NEWER
-            [UnityEngine.Scripting.Preserve]
-#endif
-            public static TweenerCore<Vector3, Path, PathOptions> CreateDOTweenPathTween(
-                MonoBehaviour target, bool tweenRigidbody, bool isLocal, Path path, float duration, PathMode pathMode
-            ){
-                TweenerCore<Vector3, Path, PathOptions> t = null;
-                bool rBodyFoundAndTweened = false;
-#if true // PHYSICS_MARKER
-                if (tweenRigidbody) {
-                    Rigidbody rBody = target.GetComponent<Rigidbody>();
-                    if (rBody != null) {
-                        rBodyFoundAndTweened = true;
-                        t = isLocal
-                            ? rBody.DOLocalPath(path, duration, pathMode)
-                            : rBody.DOPath(path, duration, pathMode);
-                    }
-                }
-#endif
-#if true // PHYSICS2D_MARKER
-                if (!rBodyFoundAndTweened && tweenRigidbody) {
-                    Rigidbody2D rBody2D = target.GetComponent<Rigidbody2D>();
-                    if (rBody2D != null) {
-                        rBodyFoundAndTweened = true;
-                        t = isLocal
-                            ? rBody2D.DOLocalPath(path, duration, pathMode)
-                            : rBody2D.DOPath(path, duration, pathMode);
-                    }
-                }
-#endif
-                if (!rBodyFoundAndTweened) {
-                    t = isLocal
-                        ? target.transform.DOLocalPath(path, duration, pathMode)
-                        : target.transform.DOPath(path, duration, pathMode);
-                }
-                return t;
-            }
+//             // Called via Reflection by DOTweenPath
+// #if UNITY_2018_1_OR_NEWER
+//             [UnityEngine.Scripting.Preserve]
+// #endif
+//             public static TweenerCore<Vector3, Path, PathOptions> CreateDOTweenPathTween(
+//                 MonoBehaviour target, bool tweenRigidbody, bool isLocal, Path path, float duration, PathMode pathMode
+//             ){
+//                 TweenerCore<Vector3, Path, PathOptions> t = null;
+//                 bool rBodyFoundAndTweened = false;
+// #if true // PHYSICS_MARKER
+//                 if (tweenRigidbody) {
+//                     Rigidbody rBody = target.GetComponent<Rigidbody>();
+//                     if (rBody != null) {
+//                         rBodyFoundAndTweened = true;
+//                         t = isLocal
+//                             ? rBody.DOLocalPath(path, duration, pathMode)
+//                             : rBody.DOPath(path, duration, pathMode);
+//                     }
+//                 }
+// #endif
+// #if true // PHYSICS2D_MARKER
+//                 if (!rBodyFoundAndTweened && tweenRigidbody) {
+//                     Rigidbody2D rBody2D = target.GetComponent<Rigidbody2D>();
+//                     if (rBody2D != null) {
+//                         rBodyFoundAndTweened = true;
+//                         t = isLocal
+//                             ? rBody2D.DOLocalPath(path, duration, pathMode)
+//                             : rBody2D.DOPath(path, duration, pathMode);
+//                     }
+//                 }
+// #endif
+//                 if (!rBodyFoundAndTweened) {
+//                     t = isLocal
+//                         ? target.transform.DOLocalPath(path, duration, pathMode)
+//                         : target.transform.DOPath(path, duration, pathMode);
+//                 }
+//                 return t;
+//             }
 
             #endregion
         }
