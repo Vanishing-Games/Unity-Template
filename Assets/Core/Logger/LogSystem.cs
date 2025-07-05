84816a28af4ed394612e160d154cb988f737e1fe
消失 <78757142+Vanish0314@users.noreply.github.com>
2025-07-05 17:04:55 +0800

    Vanish/sample odin editor (#41)
    * feat: OdinEditorSamples
    
    * feat: Intergrate NuGetForUnity
    
    * feat: Import Scriban
    refactor: change way to restore nuget packages
    
    * feat(Logger): Verbose Level & Filtering
    
    * feat: OdinComplexPageSample
    
    * fix : build codeunfucker bat
    
    * feat: add more odin samples

--- Assets/Core/Logger/LogSystem.cs
+++ Assets/Core/Logger/LogSystem.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
