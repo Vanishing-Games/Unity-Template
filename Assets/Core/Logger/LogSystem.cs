da9c20fad2bba01ff0f1674a584bc5dedcd92afa
Vanish0314 <2013107081@qq.com>
2025-07-05 17:28:15 +0800

    build: window & macos

--- Assets/Core/Logger/LogSystem.cs
+++ Assets/Core/Logger/LogSystem.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
