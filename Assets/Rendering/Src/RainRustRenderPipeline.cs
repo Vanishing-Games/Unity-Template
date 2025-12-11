8d5d24cdfd166c47e0fcf8f665a37b5a90714f4b
vanish <78757142+Vanish0314@users.noreply.github.com>
2025-12-11 21:01:14 +0800

    basic rp

---
+++ Assets/Rendering/Src/RainRustRenderPipeline.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace RainRustRenderPipeline
+{
+    public class RainRustRenderPipeline : RenderPipeline
+    {
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Rendering/Src/RainRustRenderPipelineAsset.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace RainRustRenderPipeline
+{ 
+    [CreateAssetMenu(menuName = "Rain Rust Render Pipeline/Rain Rust Render Pipeline Asset")]
+    public class RainRustRenderPipelineAsset : RenderPipelineAsset
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
