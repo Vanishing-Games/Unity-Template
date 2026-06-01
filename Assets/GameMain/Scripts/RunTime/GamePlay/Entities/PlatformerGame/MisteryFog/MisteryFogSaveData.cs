ffd322ed62cd93e94900f2b79202f186bdb7429b
Vanish <X.KRLUF@GMAIL.COM>
2026-06-01 18:45:08 +0800

    mistery fog (#341)
    * mistery fog
    
    * update
    
    * format

---
+++ Assets/GameMain/Scripts/RunTime/GamePlay/Entities/PlatformerGame/MisteryFog/MisteryFogSaveData.cs
@@ -0,0 +1,13 @@
+using System;
+using DG.Tweening;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace GameMain.RunTime
+{
+    [Serializable]
+    public class MisteryFogSaveData
+    {
+        public bool IsRevealed;
+    }
+}
