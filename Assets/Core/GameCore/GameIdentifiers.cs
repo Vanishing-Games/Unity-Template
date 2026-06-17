8d98bd4c690e68721d16ef312c549f8d2070b317
vanish <2013107081@qq.com>
2026-06-18 02:11:55 +0800

    update

---
+++ Assets/Core/GameCore/GameIdentifiers.cs
@@ -0,0 +1,15 @@
+namespace Core
+{
+    public static class GameIdentifiers
+    {
+        public static class SavePoints
+        {
+            public const string NewGame = "Chapter1Start";
+        }
+
+        public static class Chapters
+        {
+            public const string Snake = "Chapter_Snake";
+        }
+    }
+}
